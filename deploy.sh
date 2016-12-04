2e9b5a2649dd67ae5be8a9c59e1efa0d768e998a
Jon Samp <sampjon@gmail.com>
2016-12-04 16:05:08 -0600

    Deploying Update

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Moving files from build to main directory'
+cp build/index.html index.html
+cp build/transformed.js transformed.js
+
+echo 'Committing and pushing to git'
+git add .
+git commit -m "Deploying Update"
+git push
+
+echo 'Deploy successful!'
